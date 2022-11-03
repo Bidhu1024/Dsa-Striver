c263b7729c056919203787a33c418a6792ffcd86
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-11-03 09:16:48 +0530

    new code

--- Q9PrintGFG_N_times.java
+++ Q9PrintGFG_N_times.java
@@ -7,7 +7,7 @@ public class Q9PrintGFG_N_times {
             return 1;
         }
         System.out.print("GFG"+" " );
-        printGfg(N-1);
+        printGfg(N-1); 
         return N;
         
          }
