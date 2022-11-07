92008c08800b2e4f3334233961c27d7c008deb3b
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-11-07 06:43:10 +0530

    Selection Sort Day7 of DSA

---
+++ Q17SelectionSort.java
@@ -0,0 +1,17 @@
+public class Q17SelectionSort {
+    void selection(int[] arr,int n){
+        for(int i = 0;i<n-1;i++){
+            int mini = i;
+            for(int j = i+1;j<n;j++){
+                if(arr[j]<arr[mini]){
+                    mini = j;
+                }
+            }
+
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+//Selection Sort
