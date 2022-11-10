ffac9aabc023a86981c97069f72924e25867f709
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-11-10 07:21:05 +0530

    ll

---
+++ Q24CheckIfArrayIsSorted.java
@@ -0,0 +1,15 @@
+public class Q24CheckIfArrayIsSorted {
+    boolean arraySortedOrNot(int[] arr, int n) {
+        
+        
+        for(int i =1;i<n;i++){
+      
+        if(arr[i]<arr[i-1]){
+            return false;
+        }
+    }
+
+        return true;
+
+}
+}
