e9227d34077010e984fefccc794fca78637a5019
BidhubhushanGahan <bidhu1024av@gmail.com>
2022-11-14 06:41:56 +0530

    Day 14 completed

---
+++ Q29LinearSearch.java
@@ -0,0 +1,17 @@
+public class Q29LinearSearch {
+    static int searchInSorted(int arr[], int n, int k)
+    {
+        
+    if(n<1) return -1;
+    
+    int count = 0;
+    
+    for(int i = 0;i<n;i++){
+        if(arr[i]==k){
+            return 1;
+        }
+    }
+    
+        return -1;
+    }
+}
