9536efffd88dad2a4cfec817fa5b592ba13e86d0
Bidhu1024 <bidhu1024av@gmail.com>
2022-11-18 06:48:22 +0530

    Day 18 started

---
+++ Q29MissingNumber.java
@@ -0,0 +1,21 @@
+public class Q29MissingNumber {
+    public int missingNumber(int[] nums) {
+        int missingValue=0;
+        for(int i=0;i<=nums.length;i++){
+            int temp=0;
+          for(int j=0;j<nums.length;j++){
+              if(nums[j]==i){
+                  
+                  temp++;
+                  break;
+              }
+              
+          }
+            if(temp==0){
+                  missingValue=i;
+              }
+            
+        }
+        return missingValue;
+    }
+}
