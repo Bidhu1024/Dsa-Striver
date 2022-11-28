b59814bb0ed1b607c6e9cc74986a77b5aea1956a
Bidhu1024 <bidhu1024av@gmail.com>
2022-11-28 08:28:22 +0530

    Day28 Q1

---
+++ Q34TwoSum.java
@@ -0,0 +1,16 @@
+public class Q34TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int [] newarr = new int[2];
+        for(int i = 0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(target==nums[i]+nums[j]){
+                    // System.out.print(nums[i]+"," +nums[j]);
+                    newarr[0] = i;
+                    newarr[1] = j;
+
+                }
+            }
+        }
+        return newarr;
+    }
+}
